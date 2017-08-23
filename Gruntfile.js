a5471969baa5fe5298d52c7179da944024a8ba6a
Sam Smith <sam@mintcanary.com>
2017-08-23 17:29:02 +0100

    started adding content and styling

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,21 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    svgstore: {
+      options: {
+        prefix: 'icon-', // This will prefix each ID
+        svg: { // will be added as attributes to the resulting SVG
+          class : 'svgstore'
+        },
+        includedemo: '{{#each icons}}<li><svg><use xlink:href="#{{name}}" /></svg><pre>&lt;svg&gt&lt;use xlink:href=&quot;#{{name}}&quot; /&gt&lt;/svg&gt</pre></li>{{/each}}',
+      },
+      default : {
+        files: {
+          'templates/includes/icons.svg': ['assets/icons/*.svg'],
+        },
+      },
+    },
+
     sass: {
       dist: {
         files: [{
@@ -32,15 +47,21 @@ module.exports = function(grunt) {
 			css: {
 				files: '**/*.scss',
 				tasks: ['sass', 'postcss:dist']
+			},
+      icons: {
+				files: 'assets/icons/*.svg',
+				tasks: ['svgstore']
 			}
 		}
   });
 
   // Load the plugins
+  grunt.loadNpmTasks('grunt-svgstore');
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-postcss');
 
+  grunt.registerTask('icons', ['svgstore']);
   grunt.registerTask('default',['watch']);
 
 };
