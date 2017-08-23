2bf12cceab06ca6ebdd2f2830602638e918210a4
Sam Smith <sam@mintcanary.com>
2017-08-23 11:06:54 +0100

    Basic Lektor site, with minimal Bootstrap

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'assets/scss',
+          src: ['*.scss'],
+          dest: 'assets/css',
+          ext: '.css'
+        }]
+      }
+    },
+
+    postcss: {
+      options: {
+        map: true,
+        processors: [
+          require('autoprefixer')
+        ]
+      },
+      dist: {
+        src: 'assets/css/*.css'
+      }
+    },
+
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass', 'postcss:dist']
+			}
+		}
+  });
+
+  // Load the plugins
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-postcss');
+
+  grunt.registerTask('default',['watch']);
+
+};
